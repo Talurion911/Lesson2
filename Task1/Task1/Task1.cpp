7c514c906954af09c225aa9f42b115cf18934e8c
Talurion <flopebob@gmail.com>
2022-05-17 11:42:30 +1000

    Task 1.1

--- Task1/Task1/Task1.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Task1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
-using namespace std;
-int main()
-{
-    printf ("Привет мир!\n");
-}
